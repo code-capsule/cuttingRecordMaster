ef6f8b73a1ca97462e185008e1b3631a8cf666a1
LinDaiDai <244508795@qq.com>
2023-01-06 23:42:40 +0800

    feat: 补充第五章节安装 Electron，启动桌面端应用的第一个案例

---
+++ src/main/index.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const window = new BrowserWindow({})
+  window.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
