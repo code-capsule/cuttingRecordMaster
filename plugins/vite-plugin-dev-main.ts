c8fa1fae9e1c92918997f8f30de833cfc390773f
彭道宽 <1063137960@qq.com>
2023-06-06 16:53:33 +0800

    Update vite-plugin-dev-main.ts

--- plugins/vite-plugin-dev-main.ts
+++ plugins/vite-plugin-dev-main.ts
@@ -1,28 +1,25 @@
-import { buildMain, mainOutPath } from "../scripts/build.main";
+import { Plugin } from 'vite';
+import { buildMain, mainOutPath } from '../scripts/build.main';
 
-export const devMainPlugin = () => {
+export const devMainPlugin = (): Plugin => {
   return {
-    name: "vite-plugin-dev-main",
+    name: 'vite-plugin-dev-main',
     configureServer(server) {
       buildMain();
-      server.httpServer.once("listening", () => {
-        let { spawn } = require("child_process");
-        let electronProcess = spawn(
-          require("electron").toString(),
-          [mainOutPath, "--inspect=9229", "--remote-debugging-port=9222"],
-          {
-            cwd: process.cwd(),
-            stdio: "inherit",
-            env: {
-              mode: 'dev'
-            },
-          }
-        );
-        electronProcess.on("close", () => {
+      server.httpServer?.once('listening', () => {
+        const { spawn } = require('child_process');
+        const electronProcess = spawn(require('electron').toString(), [mainOutPath, '--inspect=9229', '--remote-debugging-port=9222'], {
+          cwd: process.cwd(),
+          stdio: 'inherit',
+          env: {
+            mode: 'dev',
+          },
+        });
+        electronProcess.on('close', () => {
           server.close();
           process.exit();
         });
-        server.httpServer.once("close", () => {
+        server.httpServer?.once('close', () => {
           electronProcess.close();
           process.exit();
         });
