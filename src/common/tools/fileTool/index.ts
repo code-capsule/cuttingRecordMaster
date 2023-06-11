0bb361444844c8b95bd68e539bb3a181888b881b
pengdaokuan <pengdaokuan@cvte.com>
2023-06-11 16:25:14 +0800

    [linted]feature: 新增FileTool工具服务

---
+++ src/common/tools/fileTool/index.ts
@@ -0,0 +1,71 @@
+/**
+ * @description 本地文件工具服务
+ */
+import fs from 'fs';
+const fsPromiseAPIs = fs.promises;
+
+class FileTool {
+  /**
+   * @description 是否能读取此文件
+   * @param path 文件路径
+   */
+  public canReadFile(path: string): Promise<void> {
+    return fsPromiseAPIs.access(path, fs.constants.R_OK);
+  }
+  /**
+   * @description 读取文件内容
+   * @param path 路径
+   * @returns {Promise}
+   */
+  public read(path: string, encoding?: BufferEncoding): Promise<string> {
+    return fsPromiseAPIs.readFile(path, { encoding: encoding || 'utf8' });
+  }
+  /**
+   * @description 是否可写入此文件
+   * @param path 路径
+   * @returns {Promise}
+   */
+  public canWrite(path: string): Promise<void> {
+    return fsPromiseAPIs.access(path, fs.constants.W_OK);
+  }
+  /**
+   * @description 写入文件内容
+   * @param path 路径
+   * @returns {Promise}
+   */
+  public write(
+    path: string,
+    content: string | NodeJS.ArrayBufferView | Iterable<string | NodeJS.ArrayBufferView> | AsyncIterable<string | NodeJS.ArrayBufferView>,
+    encoding?: BufferEncoding
+  ): Promise<void> {
+    const updateContent = typeof content === 'string' ? content : JSON.stringify(content);
+    return fsPromiseAPIs.writeFile(path, updateContent, { encoding: encoding || 'utf8' });
+  }
+  /**
+   * @description 是否存在文件
+   * @param path 路径
+   * @returns {Promise}
+   */
+  public hasFile(path: string): Promise<void> {
+    return fsPromiseAPIs.access(path, fs.constants.F_OK);
+  }
+  /**
+   * @description 创建文件夹
+   * @param path 创建 /a/b/c，不管`/a` 和 /a/b 是否存在。
+   * @returns {Promise}
+   */
+  public mkdirDir(path: string): Promise<string | undefined> {
+    return fsPromiseAPIs.mkdir(path, { recursive: true });
+  }
+
+  /**
+   * @description 读取文件夹目录
+   * @param path 路径
+   * @returns {Promise<string[]>}
+   */
+  public readdirSync(path: string): Promise<string[]> {
+    return fsPromiseAPIs.readdir(path);
+  }
+}
+
+export default new FileTool() as FileTool;
