b16bfa160abf9641134d9605e2fb61a189149e7c
LinDaiDai <244508795@qq.com>
2023-06-15 19:22:26 +0800

    modify: 调整主进程中 master 初始化的方式

--- src/main/setup/index.ts
+++ src/main/setup/index.ts
@@ -1,17 +1,41 @@
-import initMaster, { InitMainMasterOptions } from './master';
 import {
   createHome,
   createRecord,
 } from '@common/services/windowService/windows';
+import { Master } from '@typings/node';
+import { MainIpc } from '@common/services/ipc';
+import WindowService from '@common/services/windowService';
+import { registerWindow } from '@common/services/windowService/windows';
+import { MAIN_PROCESS_KEY } from '@common/constants/processKey';
+import initMainLog from './log';
+import initReduxStore from '@common/stores/reduxStore';
 
-interface SetUpMainOptions extends InitMainMasterOptions {}
+interface SetUpMainOptions {}
 
 export default async function setupMain(
   options?: SetUpMainOptions
 ): Promise<void> {
   require('@electron/remote/main').initialize();
 
-  const master = await initMaster(options);
+  const ipc = new MainIpc({ processKey: MAIN_PROCESS_KEY });
+  const log = initMainLog();
+  const windowService = WindowService;
+  registerWindow();
+
+  const reduxStore = initReduxStore();
+
+  const master: Master = {
+    services: {
+      ipc,
+      windowService,
+    },
+    tools: {
+      log,
+    },
+    stores: {
+      reduxStore,
+    },
+  };
   global.master = master;
 
   global.master.tools.log.info('the main process started successfully!');
