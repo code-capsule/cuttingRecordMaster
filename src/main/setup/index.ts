7fa3c3049752177708d8ae98f7ff1daf49083a26
LinDaiDai <244508795@qq.com>
2023-06-15 19:16:24 +0800

    modify: 修改主进程中的 master 初始化逻辑

--- src/main/setup/index.ts
+++ src/main/setup/index.ts
@@ -1,17 +1,36 @@
-import initMaster, { InitMainMasterOptions } from './master';
 import {
   createHome,
   createRecord,
 } from '@common/services/windowService/windows';
+import { Master } from '@typings/node';
+import { MainIpc } from '@common/services/ipc';
+import WindowService from '@common/services/windowService';
+import { registerWindow } from '@common/services/windowService/windows';
+import { MAIN_PROCESS_KEY } from '@common/constants/processKey';
+import initMainLog from './log';
 
-interface SetUpMainOptions extends InitMainMasterOptions {}
+interface SetUpMainOptions {}
 
 export default async function setupMain(
   options?: SetUpMainOptions
 ): Promise<void> {
   require('@electron/remote/main').initialize();
 
-  const master = await initMaster(options);
+  const ipc = new MainIpc({ processKey: MAIN_PROCESS_KEY });
+  const log = initMainLog();
+  const windowService = WindowService;
+  registerWindow();
+
+  const master: Master = {
+    services: {
+      ipc,
+      windowService,
+    },
+    tools: {
+      log,
+    },
+  };
+
   global.master = master;
 
   global.master.tools.log.info('the main process started successfully!');
