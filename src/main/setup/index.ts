88b229f515d355226fcc977a9350eeefe46d354c
pengdaokuan <pengdaokuan@cvte.com>
2023-06-16 21:00:15 +0800

    [linted]feature: 优化setup逻辑

--- src/main/setup/index.ts
+++ src/main/setup/index.ts
@@ -1,8 +1,5 @@
-import {
-  createHome,
-  createRecord,
-} from '@common/services/windowService/windows';
-import { Master } from '@typings/node';
+import { createHome, createRecord } from '@common/services/windowService/windows';
+import { Master, MasterServices, MasterTools } from '@typings/node';
 import { MainIpc } from '@common/services/ipc';
 import WindowService from '@common/services/windowService';
 import { registerWindow } from '@common/services/windowService/windows';
@@ -11,27 +8,26 @@ import initMainLog from './log';
 
 interface SetUpMainOptions {}
 
-export default async function setupMain(
-  options?: SetUpMainOptions
-): Promise<void> {
+export default async function setupMain(options?: SetUpMainOptions): Promise<void> {
+  // remote模块注入
   require('@electron/remote/main').initialize();
+  global.master = {} as Master;
+  global.master.tools = {} as MasterTools;
+  global.master.services = {} as MasterServices;
 
+  // 挂载通信服务
   const ipc = new MainIpc({ processKey: MAIN_PROCESS_KEY });
-  const log = initMainLog();
+  global.master.services.ipc = ipc;
+
+  // 挂载窗口管理服务
   const windowService = WindowService;
+  global.master.services.windowService = windowService;
+
+  // 挂载日志工具
+  const log = initMainLog();
+  global.master.tools.log = log;
+
   registerWindow();
-
-  const master: Master = {
-    services: {
-      ipc,
-      windowService,
-    },
-    tools: {
-      log,
-    },
-  };
-
-  global.master = master;
 
   global.master.tools.log.info('the main process started successfully!');
 
