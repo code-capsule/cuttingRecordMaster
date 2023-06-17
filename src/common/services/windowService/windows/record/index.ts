be2deaf8d516a47d186a53b1cbf43f35daab0305
LinDaiDai <244508795@qq.com>
2023-06-17 11:38:16 +0800

    feat: 初步实现录制页

--- src/common/services/windowService/windows/record/index.ts
+++ src/common/services/windowService/windows/record/index.ts
@@ -3,8 +3,13 @@ import { RECORD_PROCESS_KEY } from '@common/constants/processKey';
 
 export function registerRecord() {
   windowService.register(RECORD_PROCESS_KEY, {
-    width: 800,
+    width: 750,
     height: 600,
+    frame: false,
+    vibrancy: 'light',
+    visualEffectState: 'active',
+    transparent: true,
+    resizable: false,
   });
 }
 
---
+++ src/render/pages/record/containers/RecordMain/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.less';
+
+const Main = () => {
+  return <div className="container">
+  <div className="title">Hello World!</div>
+</div>;
+};
+
+export default Main;
