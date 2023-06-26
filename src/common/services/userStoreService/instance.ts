ab6625b9b8923c6b427eb9887dfbf6bcc8c727e6
pengdaokuan <pengdaokuan@cvte.com>
2023-06-26 14:18:09 +0800

    feature: 完成userStoreService功能，1、本地json文件的生成；2、应用数据与本地数据的同步

--- src/common/services/userStoreService/instance.ts
+++ src/common/services/userStoreService/instance.ts
@@ -1,16 +1,41 @@
+import CuttingRecordMasterElectronStore from '@common/stores/localStore';
+import { ToolkitStore } from '@reduxjs/toolkit/dist/configureStore';
+import { userPageSlice } from '@common/stores/reduxStore/reducers/userPage';
+import { initialUserState } from '@common/stores/reduxStore/reducers/userPage';
+
+interface IProps {
+  reduxStore: ToolkitStore;
+  localStore?: CuttingRecordMasterElectronStore;
+}
+
 class UserStore {
+  reduxStore?: ToolkitStore;
+  localStore?: CuttingRecordMasterElectronStore;
+
+  constructor(props: IProps) {
+    this.reduxStore = props.reduxStore;
+    this.localStore = props.localStore;
+  }
+
   /**
    * @description 获取用户信息
    */
-  public getUserInfo() {
+  public getUserInfo(): MasterUserType.IUserInfo {
     console.log('[userStoreService] get user info');
+    return this.localStore?.get('userInfo') as MasterUserType.IUserInfo;
   }
 
   /**
    * @description 更新用户信息
+   * @param {IUserDataInfo} userInfo 用户信息
    */
-  public updateUserInfo() {
+  public updateUserInfo(userInfo: MasterUserType.IUserInfo) {
     console.log('[userStoreService] update user info');
+    this.reduxStore?.dispatch(userPageSlice?.actions?.updateUserInfo({ ...userPageSlice.getInitialState(), ...userInfo }));
+    // 同步更新本地json
+    this.localStore?.set({
+      userInfo: { ...initialUserState, ...userInfo },
+    });
   }
 
   /**
@@ -18,6 +43,11 @@ class UserStore {
    */
   public overrideUserInfo() {
     console.log('[userStoreService] override user info');
+    this.reduxStore?.dispatch(userPageSlice?.actions?.updateUserInfo(initialUserState));
+    // 同步更新本地json
+    this.localStore?.set({
+      userInfo: initialUserState,
+    });
   }
 }
 
