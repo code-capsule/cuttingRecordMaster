a266b3da34939327d4795b3986d1a08a21fff67c
pengdaokuan <pengdaokuan@cvte.com>
2023-06-26 14:17:17 +0800

    feature: 新增reduxStore中的用户信息模块

--- src/common/stores/reduxStore/reducers/index.ts
+++ src/common/stores/reduxStore/reducers/index.ts
@@ -1,11 +1,14 @@
 import { recordPageSlice, recordPage } from './recordPage';
+import { userPageSlice, initialUserState } from './userPage';
 
 export const initialState: MasterAppStoreType.AppState = {
   recordPage,
+  userPage: initialUserState
 };
 
 export default function initialReducers() {
   return {
     recordPage: recordPageSlice.reducer,
+    userPage: userPageSlice.reducer
   };
 }
