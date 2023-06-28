81782193b687afe315b91378de0a415b3a965e88
pengdaokuan <pengdaokuan@cvte.com>
2023-06-28 15:55:38 +0800

    feature: 完成首页右侧的内容展示，完成录制和剪辑的入口样式

---
+++ src/render/pages/home/containers/CuttingRecordingBox/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.less';
+const CuttingRecordingBox = () => {
+  return (
+    <div className="home-cutting-record-box">
+        <div className="home-box home-recording">
+          <div className="label">Record</div>
+        </div>
+        <div className="home-box home-cutting">
+          <div className="label">Clip</div>
+        </div>
+    </div>
+  );
+};
+
+export default CuttingRecordingBox;
--- src/render/pages/home/containers/index.tsx
+++ src/render/pages/home/containers/index.tsx
@@ -2,23 +2,29 @@ import React, { useState } from 'react';
 import './index.less';
 import User from './User';
 import Sidebar from './Sidebar';
+import TopMenu from '@common/components/TopMenu';
+import Scrollbar from '@common/components/ScrollBar';
 import { SideMenuType } from './Sidebar/constant';
 import { HOME_PROCESS_KEY } from '@common/constants/processKey';
-import TopMenu from '@common/components/TopMenu';
-
+import CuttingRecordingBox from './CuttingRecordingBox';
 
 function Main() {
   const [sideMenu, setSideMenu] = useState<SideMenuType>(SideMenuType.DISCOVER);
 
   return (
     <div className="home-container">
-    <div className="home-drag-box" />
+      <div className="home-drag-box" />
       <div className="home-top-menu">
         <TopMenu currentWindow={HOME_PROCESS_KEY} svgColor="#808191" />
       </div>
-      <div className='home-left'>
+      <div className="home-left">
         <Sidebar currentSideMenu={sideMenu} onChange={setSideMenu} />
         <User />
+      </div>
+      <div className="home-right">
+        <Scrollbar maxHeight={window.innerHeight - 32} x={{ show: false }} y={{ show: false }}>
+          <CuttingRecordingBox />
+        </Scrollbar>
       </div>
     </div>
   );
