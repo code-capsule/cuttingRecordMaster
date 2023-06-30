2bb28bd8c424f9c0618bc1db616846a9a0aebf61
pengdaokuan <pengdaokuan@cvte.com>
2023-06-30 11:41:24 +0800

    [linted]feature: 新增工程相关的IPC事件名

--- src/common/constants/ipcEventKey.ts
+++ src/common/constants/ipcEventKey.ts
@@ -6,3 +6,9 @@
 export const WINDOW_IPC_KEY = {
   OPEN_HOME_WINDOW: 'windowService.open.home.window', // 打开首页窗口
 };
+
+// 工程数据相关
+export const PROJECT_STORE_IPC_KEY = {
+  INIT_PROJECT_STORE: 'INIT_PROJECT_STORE', // 初始化工程
+  UNMOUNT_PROJECT_STORE: 'UNMOUNT_PROJECT_STORE', // 卸载工程
+};
