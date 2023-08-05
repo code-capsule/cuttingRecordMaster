a0cac21e76e76ab10cc28849c363fc227c0de125
LinDaiDai <244508795@qq.com>
2023-08-05 23:45:53 +0800

    feat: 新增 WindowLoading 公共组件

---
+++ src/common/components/WindowLoading/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.less';
+
+const WindowLoading = () => {
+  return (
+    <div className="window-loading-box">
+      <div className="window-loading"></div>
+    </div>
+  );
+};
+
+export default WindowLoading;
