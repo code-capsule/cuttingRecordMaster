f02d0ac7c27808b79e92912c4ae43d5615216b55
pengdaokuan <pengdaokuan@cvte.com>
2023-08-06 13:07:24 +0800

    [linted]feature: 格式化代码

--- src/render/pages/clip/containers/index.tsx
+++ src/render/pages/clip/containers/index.tsx
@@ -6,12 +6,12 @@ const Main = () => {
   return (
     <div className="clip-main-container">
       <Frame
-      renderHeaderComponent={<div>header</div>}
-      renderLeftComponent={<div>resource</div>}
-      renderCenterComponent={<div>player</div>}
-      renderRightComponent={<div>info</div>}
-      renderBottomComponent={<div>track</div>}
-    />
+        renderHeaderComponent={<div>header</div>}
+        renderLeftComponent={<div>resource</div>}
+        renderCenterComponent={<div>player</div>}
+        renderRightComponent={<div>info</div>}
+        renderBottomComponent={<div>track</div>}
+      />
     </div>
   );
 };
