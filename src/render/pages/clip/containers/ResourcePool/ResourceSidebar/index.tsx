588dbd89cbb7f968ee070552218b5e0ac8c3c329
pengdaokuan <pengdaokuan@cvte.com>
2023-08-09 09:36:37 +0800

    [linted]feature: 初步完成资源池的 ResourceSidebar 和 ResourceTab 组件

---
+++ src/render/pages/clip/containers/ResourcePool/ResourceSidebar/index.tsx
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import './index.less';
+import { ResourcePoolContext, ResourceContextProps } from '@render/pages/clip/containers/ResourcePool';
+import { ReactComponent as IcCommonVideoSvg } from '@common/svgs/ic_common_video.svg';
+import { ReactComponent as IcCommonAudioSvg } from '@common/svgs/ic_common_audio.svg';
+import { ReactComponent as IcCommonImageSvg } from '@common/svgs/ic_common_image.svg';
+import { ReactComponent as IcCommonTextSvg } from '@common/svgs/ic_common_text.svg';
+import { EResourceType } from '@src/typings/resource/enum';
+
+const ResourceSidebar = () => {
+  const resourceContextReducer = useContext<ResourceContextProps>(ResourcePoolContext);
+
+  return (
+    <div className="clip-resource-sidebar">
+      <div className="clip-resource-item" />
+      <div
+        className={`clip-resource-item ${resourceContextReducer.sidebarType === EResourceType.video ? 'active' : ''}`}
+        onClick={() => resourceContextReducer?.updateSidebarType?.(EResourceType.video)}
+      >
+        <IcCommonVideoSvg className="resource-svg" />
+      </div>
+      <div
+        className={`clip-resource-item ${resourceContextReducer.sidebarType === EResourceType.audio ? 'active' : ''}`}
+        onClick={() => resourceContextReducer?.updateSidebarType?.(EResourceType.audio)}
+      >
+        <IcCommonAudioSvg className="resource-svg" />
+      </div>
+      <div
+        className={`clip-resource-item ${resourceContextReducer.sidebarType === EResourceType.text ? 'active' : ''}`}
+        onClick={() => resourceContextReducer?.updateSidebarType?.(EResourceType.text)}
+      >
+        <IcCommonTextSvg className="resource-svg" />
+      </div>
+      <div
+        className={`clip-resource-item ${resourceContextReducer.sidebarType === EResourceType.image ? 'active' : ''}`}
+        onClick={() => resourceContextReducer?.updateSidebarType?.(EResourceType.image)}
+      >
+        <IcCommonImageSvg className="resource-svg" />
+      </div>
+    </div>
+  );
+};
+
+export default ResourceSidebar;
---
+++ src/render/pages/clip/containers/ResourcePool/ResourceTab/index.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import './index.less';
+import { EResourceFromType } from '@src/typings/resource/enum';
+import { ResourcePoolContext, ResourceContextProps } from '@render/pages/clip/containers/ResourcePool';
+
+const ResourceTab = () => {
+  const resourceContextReducer = useContext<ResourceContextProps>(ResourcePoolContext);
+
+  return (
+    <div className="clip-resource-tab">
+      <div
+        className={`clip-resource-tab-item ${resourceContextReducer?.tabType === EResourceFromType.template ? 'active' : ''}`}
+        onClick={() => resourceContextReducer?.updateTabType?.(EResourceFromType.template)}
+      >
+        云模板
+      </div>
+      <div
+        className={`clip-resource-tab-item ${resourceContextReducer?.tabType === EResourceFromType.local ? 'active' : ''}`}
+        onClick={() => resourceContextReducer?.updateTabType?.(EResourceFromType.local)}
+      >
+        本地文件
+      </div>
+    </div>
+  );
+};
+
+export default ResourceTab;
