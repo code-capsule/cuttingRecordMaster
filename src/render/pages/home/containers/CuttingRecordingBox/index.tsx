0a7b52e6d1937aff2fb4af4d509ca95b86fd1628
pengdaokuan <pengdaokuan@cvte.com>
2023-10-21 17:52:35 +0800

    [linted]feature: 修改内容和调整代码

--- src/render/pages/home/containers/CuttingRecordingBox/index.tsx
+++ src/render/pages/home/containers/CuttingRecordingBox/index.tsx
@@ -3,12 +3,12 @@ import './index.less';
 const CuttingRecordingBox = () => {
   return (
     <div className="home-cutting-record-box">
-        <div className="home-box home-recording">
-          <div className="label">Record</div>
-        </div>
-        <div className="home-box home-cutting">
-          <div className="label">Clip</div>
-        </div>
+      <div className="home-box home-recording">
+        <div className="label">录制</div>
+      </div>
+      <div className="home-box home-cutting">
+        <div className="label">创作</div>
+      </div>
     </div>
   );
 };
--- src/render/pages/home/containers/index.tsx
+++ src/render/pages/home/containers/index.tsx
@@ -13,8 +13,7 @@ const DRAFT_LIST: MasterDraftType.IDraftItem[] = [
   {
     id: '111',
     projectName: 'Basic how to ride your skateboard comfortly',
-    firstVideoPool:
-      'https://player.vimeo.com/external/436572488.sd.mp4?s=eae5fb490e214deb9ff532dd98d101efe94e7a8b&profile_id=139&oauth2_token_id=57447761',
+    videoUrl: 'https://player.vimeo.com/external/436572488.sd.mp4?s=eae5fb490e214deb9ff532dd98d101efe94e7a8b&profile_id=139&oauth2_token_id=57447761',
     duration: 351,
     createTime: 1687943237882,
     updateTime: 1687943237882,
@@ -22,8 +21,7 @@ const DRAFT_LIST: MasterDraftType.IDraftItem[] = [
   {
     id: '222',
     projectName: 'Prepare for your first skateboard jump',
-    firstVideoPool:
-      'https://player.vimeo.com/external/449972745.sd.mp4?s=9943177fe8a6147b7bc4598259401f06ec57878a&profile_id=139&oauth2_token_id=57447761',
+    videoUrl: 'https://player.vimeo.com/external/449972745.sd.mp4?s=9943177fe8a6147b7bc4598259401f06ec57878a&profile_id=139&oauth2_token_id=57447761',
     duration: 351,
     createTime: 1687943237882,
     updateTime: 1687943237882,
@@ -31,8 +29,7 @@ const DRAFT_LIST: MasterDraftType.IDraftItem[] = [
   {
     id: '333',
     projectName: 'Basic equipment to play skateboard safely',
-    firstVideoPool:
-      'https://player.vimeo.com/external/436553499.sd.mp4?s=0e44527f269278743db448761e35c5e39cfaa52c&profile_id=139&oauth2_token_id=57447761',
+    videoUrl: 'https://player.vimeo.com/external/436553499.sd.mp4?s=0e44527f269278743db448761e35c5e39cfaa52c&profile_id=139&oauth2_token_id=57447761',
     duration: 351,
     createTime: 1687943237882,
     updateTime: 1687943237882,
@@ -40,8 +37,7 @@ const DRAFT_LIST: MasterDraftType.IDraftItem[] = [
   {
     id: '444',
     projectName: 'Tips to playing skateboard on the ramp',
-    firstVideoPool:
-      'https://player.vimeo.com/external/361861493.sd.mp4?s=19d8275ca755d653042a87ef28b2f0b2eabf57d0&profile_id=139&oauth2_token_id=57447761',
+    videoUrl: 'https://player.vimeo.com/external/361861493.sd.mp4?s=19d8275ca755d653042a87ef28b2f0b2eabf57d0&profile_id=139&oauth2_token_id=57447761',
     duration: 351,
     createTime: 1687943237882,
     updateTime: 1687943237882,
@@ -64,7 +60,7 @@ function Main() {
       <div className="home-right">
         <Scrollbar maxHeight={window.innerHeight - 32} x={{ show: false }} y={{ show: false }}>
           <CuttingRecordingBox />
-          <div className="home-product-title">Recent Draft Product </div>
+          <div className="home-product-title">最近作品 </div>
           <div className="home-product-list">
             {DRAFT_LIST?.map((draftInfo: MasterDraftType.IDraftItem, idx) => {
               return (
