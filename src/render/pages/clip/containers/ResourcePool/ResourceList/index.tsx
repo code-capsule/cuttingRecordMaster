a64a56e08d1a2ea00e4d2919cf70255faba622c0
pengdaokuan <pengdaokuan@cvte.com>
2023-10-24 17:48:27 +0800

    [linted]feature: 完成无视频素材时的样式

--- src/render/pages/clip/containers/ResourcePool/ResourceList/index.tsx
+++ src/render/pages/clip/containers/ResourcePool/ResourceList/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import './index.less';
 import { useSelector, shallowEqual } from 'react-redux';
 import { ResourcePoolContext, ResourceContextProps } from '@render/pages/clip/containers/ResourcePool';
 import { EResourceType } from '@src/typings/resource/enum';
@@ -6,14 +7,27 @@ import VideoList from './VideoList';
 import ImageList from './ImageList';
 import TextList from './TextList';
 import MATERIAL_CONSTANTS from '@src/assets/material';
+import { ReactComponent as IcCommonVideoSvg } from '@common/svgs/ic_common_video.svg';
 
 const ResourceList = () => {
   const resourceContextReducer = useContext<ResourceContextProps>(ResourcePoolContext);
-  const material = useSelector((store: MasterAppStoreType.AppState) => store?.projectPage?.material || {}, shallowEqual);
+  const videoMaterial = useSelector((store: MasterAppStoreType.AppState) => store?.projectPage?.material?.video || [], shallowEqual);
 
   return (
     <React.Fragment>
-      {resourceContextReducer?.sidebarType === EResourceType.video && <VideoList data={material?.video || []} />}
+      {resourceContextReducer?.sidebarType === EResourceType.video && (
+        <React.Fragment>
+          {videoMaterial?.length > 0 && <VideoList data={videoMaterial || []} />}
+          {videoMaterial?.length === 0 && (
+            <div className="empty-resource-container">
+              <div className="empty-resource-button">
+                <IcCommonVideoSvg className="empty-resource-svg" />
+                录制素材
+              </div>
+            </div>
+          )}
+        </React.Fragment>
+      )}
       {resourceContextReducer?.sidebarType === EResourceType.image && <ImageList data={MATERIAL_CONSTANTS?.image || []} />}
       {resourceContextReducer?.sidebarType === EResourceType.text && <TextList data={MATERIAL_CONSTANTS?.text || []} />}
     </React.Fragment>
