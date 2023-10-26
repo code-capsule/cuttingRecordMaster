ff7a9ab485b961e3e44dd096f0310019efa671ca
pengdaokuan <pengdaokuan@cvte.com>
2023-10-26 11:50:35 +0800

    [linted]feature: 往轨道区 provider div 上挂 ref ，目的是为了拿到轨道区可视宽度

--- src/render/pages/clip/containers/Track/index.tsx
+++ src/render/pages/clip/containers/Track/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './index.less';
 import { useSelector, shallowEqual } from 'react-redux';
 import Toolbar from './ToolBar';
 
 const Track = React.memo(() => {
+  const providerElementRef = useRef<HTMLDivElement>(null);
   const videoMaterial = useSelector((store: MasterAppStoreType.AppState) => store?.projectPage?.material?.video || [], shallowEqual);
 
   return (
@@ -13,7 +14,7 @@ const Track = React.memo(() => {
       </div>
       <div className="clip-provider-container">
         <div className="clip-track-wrapper-outer" style={{ paddingLeft: 16 }} onClick={() => {}}>
-          <div className="clip-track-wrapper-inner" id="clip-track-wrapper-inner">
+          <div className="clip-track-wrapper-inner" id="clip-track-wrapper-inner" ref={providerElementRef}>
             {videoMaterial?.[0]?.data?.thumbnails?.map((url, idx) => {
               return (
                 <div key={idx} className="clip-track-video-cell-item">
