7617915f17236e5603e10af86421f6dfc637a2cd
pengdaokuan <pengdaokuan@cvte.com>
2023-10-26 11:28:37 +0800

    [linted]feature: 轨道区的样式修改，接入工具条组件

--- src/render/pages/clip/containers/Track/index.tsx
+++ src/render/pages/clip/containers/Track/index.tsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import './index.less';
 import { useSelector, shallowEqual } from 'react-redux';
+import Toolbar from './ToolBar';
 
 const Track = React.memo(() => {
   const videoMaterial = useSelector((store: MasterAppStoreType.AppState) => store?.projectPage?.material?.video || [], shallowEqual);
 
   return (
     <div className="clip-track-container">
-      <div className="clip-track-wrapper-outer" style={{ paddingLeft: 16 }} onClick={() => {}}>
-        <div className="clip-track-wrapper-inner" id="clip-track-wrapper-inner">
-          {videoMaterial?.[0]?.data?.thumbnails?.map((url, idx) => {
-            return (
-              <div key={idx} className="clip-track-video-cell-item">
-                <img src={url} className="clip-track-video-cell-item-cover" />
-              </div>
-            );
-          })}
+      <div className="clip-toolbar-container">
+        <Toolbar />
+      </div>
+      <div className="clip-provider-container">
+        <div className="clip-track-wrapper-outer" style={{ paddingLeft: 16 }} onClick={() => {}}>
+          <div className="clip-track-wrapper-inner" id="clip-track-wrapper-inner">
+            {videoMaterial?.[0]?.data?.thumbnails?.map((url, idx) => {
+              return (
+                <div key={idx} className="clip-track-video-cell-item">
+                  <img src={url} className="clip-track-video-cell-item-cover" />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
