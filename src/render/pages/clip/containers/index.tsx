9c2a2b9c19495cf8383ed5823cc393a1e93b59bf
pengdaokuan <pengdaokuan@cvte.com>
2023-10-26 09:32:04 +0800

    [linted]feature: 初步完成轨道区的显示，将视频的缩略图列表进行展示

--- src/render/pages/clip/containers/index.tsx
+++ src/render/pages/clip/containers/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './index.less';
 import Frame from '@render/pages/clip/components/Frame';
 import Header from '@render/pages/clip/containers/Header';
+import Track from '@render/pages/clip/containers/Track';
 import MaterialCenter from '@render/pages/clip/containers/MaterialCenter';
 
 const Main = () => {
@@ -12,7 +13,7 @@ const Main = () => {
         renderLeftComponent={<MaterialCenter />}
         renderCenterComponent={<div>player</div>}
         renderRightComponent={<div>info</div>}
-        renderBottomComponent={<div>track</div>}
+        renderBottomComponent={<Track />}
       />
     </div>
   );
