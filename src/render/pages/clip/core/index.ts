5d8059bfd34a6f3e09d812d5577b639a3ad2e950
pengdaokuan <pengdaokuan@cvte.com>
2023-12-10 17:11:31 +0800

    feature: 新增trackManager轨道管理模块

--- src/render/pages/clip/core/index.ts
+++ src/render/pages/clip/core/index.ts
@@ -1,7 +1,9 @@
 import mediaManager from './mediaManager';
+import trackManager from './trackManager';
 import materialManager from './materialManager';
 
 export default {
   mediaManager,
+  trackManager,
   materialManager,
 };
